8492e85b70e1c9fada632ecaf02eab2a6b197e02
Ismar M <ismar.mujezinovic@icloud.com>
2025-12-13 13:39:26 +0100

    Dodal unit teste za UPDATE + REMINDER. Dodal mapo za testiranje in datoteko porocilo_testiranje.md ter vnesel svoje rezultate testiranja.

---
+++ backend/src/test/java/com/example/todo/task/TaskServiceReminderTest.java
@@ -0,0 +1,4 @@
+package com.example.todo.task;
+
+public class TaskServiceReminderTest {
+}
---
+++ backend/src/test/java/com/example/todo/task/TaskServiceUpdateTest.java
@@ -0,0 +1,19 @@
+package com.example.todo.task;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+
+public class TaskServiceUpdateTest {
+}
